8d4b122772459f937abdcde40dfef452359bc177
AnstMykh <99489970+AnstMykh@users.noreply.github.com>
2024-11-05 15:56:19 +0100

    Create LD_in_windows.R

---
+++ stats/LD_in_windows.R
@@ -0,0 +1,229 @@
+#could take a lot of time, so running on the background
+### loading packages
+if (!require("gplots")) {
+  install.packages("gplots", dependencies = TRUE)
+  library(gplots)
+}
+if (!require("lattice")) {
+  install.packages("lattice", dependencies = TRUE)
+  library(lattice)
+}
+
+
+setwd("~/mut_load/OUTPUT/outliers")
+path_to_file <- ""
+path_to_output <- ""
+list_of_data_files <- dir(path_to_file, pattern="*.ld", all.files=FALSE, full.names=FALSE)
+list_of_pos_files <- dir(path_to_file, pattern="*.pos.txt", all.files=FALSE, full.names=FALSE)
+
+
+
+for (filename in list_of_data_files) {
+  file_name_no_ext <- tools::file_path_sans_ext(filename)
+  data_filename <- file.path(path_to_file, paste(file_name_no_ext, "ld", sep="."))
+  ld_contig <- read.table(data_filename, strip.white = TRUE, header = FALSE, stringsAsFactors=FALSE)
+  
+  
+  positions_filename <- file.path(path_to_file, paste(file_name_no_ext, "pos","txt", sep="."))
+  SNP_positions <- read.table(positions_filename, header = TRUE)
+  SNP_positions <- as.vector(SNP_positions[, 1])
+  
+  
+  #matrix of windows of 10 kb
+  max_kb <- SNP_positions[length(SNP_positions)]
+  window_size <- 10000
+  window_max <- ceiling(max_kb/window_size) 
+  
+  column_names <- c()
+  row_names <- c()
+  ld_matrix <- matrix( 0, nrow = window_max, ncol = window_max)
+  
+  
+  for (window_x in 1:window_max) {
+    for (window_y in window_x:window_max) {
+      col_x_start <- (window_x - 1) * window_size + 1 
+      col_x_end <- (window_x) * window_size
+      
+      col_y_start <- (window_y - 1) * window_size + 1
+      col_y_end <- (window_y) * window_size
+      
+      #print(which(SNP_positions >= col_x_start & SNP_positions < col_x_end))
+      #print(which(SNP_positions >= col_y_start & SNP_positions < col_y_end))
+      x_indeces <- which(SNP_positions >= col_x_start & SNP_positions < col_x_end)
+      y_indeces <- which(SNP_positions >= col_y_start & SNP_positions < col_y_end)
+      ld_contig_cut <- ld_contig[y_indeces , x_indeces]
+      if (length(x_indeces) == 0 | length(y_indeces) == 0) {
+        R2_2nd <- 0
+      } else {
+        R2_2nd <- sapply(ld_contig_cut, max)
+        R2_2nd <- sort(as.vector(R2_2nd), decreasing = T)
+        # print(c(window_x, window_y, R2_2nd))
+        if (length(R2_2nd) == 1) {
+          R2_2nd <- R2_2nd[1]
+        } else {
+          R2_2nd <- R2_2nd[2]
+        }
+      }
+      row_names <- append(row_names, paste(col_y_end))
+      row_names <- unique(row_names)
+      ld_matrix[window_y, window_x] <- R2_2nd
+      ld_matrix[window_x, window_y] <- R2_2nd
+      
+    }
+    column_names <- append(column_names, paste(col_x_end))
+  }
+  
+  colnames(ld_matrix) <- c(column_names)
+  rownames(ld_matrix) <-c(row_names)
+  
+  write.table(ld_matrix, file = paste(path_to_output,file_name_no_ext, '.txt'  ,sep = ''))
+  
+}
+
+## now for creating a matrix where the upper triangle is LD for only homozygous (one of the haplotypes) 
+individuals and the lover triange includes all
+
+
+
+path_to_file <- "" 
+path_to_output <- ""
+path_to_contig_ld <- ""
+list_of_data_files <- dir(path_to_file, pattern="*\\.ld", all.files=FALSE, full.names=FALSE)
+list_of_pos_files <- dir(path_to_file, pattern="*\\.pos.txt", all.files=FALSE, full.names=FALSE)
+
+
+for (filename in list_of_data_files) {
+  file_name_no_ext <- tools::file_path_sans_ext(filename)
+  contig <- str_split(file_name_no_ext, "_")[[1]] [1]
+  
+  #data from the homozygous  sample 
+  data_filename_inv <- file.path(path_to_file, paste(file_name_no_ext, "ld", sep="."))
+  ld_inv <- read.table(data_filename_inv, strip.white = TRUE, header = FALSE, stringsAsFactors=FALSE)
+  #data for the whole inversion 
+  data_filename_contig <- file.path(path_to_contig_ld, paste(contig, "ld", sep="."))
+  ld_contig <- read.table(data_filename_contig, strip.white = TRUE, header = FALSE, stringsAsFactors=FALSE)
+  
+  
+  #SNP positions for the homozygous sample
+  positions_filename_inv <- file.path(path_to_file, paste(file_name_no_ext, "pos","txt", sep="."))
+  SNP_positions_inv <- read.table(positions_filename_inv, header = TRUE)
+  SNP_positions_inv <- as.vector(SNP_positions_inv[, 1])
+  #SNP positions for the whole inversion 
+  positions_filename_contig <- file.path(path_to_contig_ld, paste(contig, "pos","txt", sep="."))
+  SNP_positions_contig <- read.table(positions_filename_contig, header = TRUE)
+  SNP_positions_contig<- as.vector(SNP_positions_contig[, 1]) 
+  
+  
+  
+  #matrix of windows of 10 kb 
+  max_kb <- max(SNP_positions_inv[length(SNP_positions_inv)],SNP_positions_contig[length(SNP_positions_contig)]) ## poklady maxymalne znachannya z dwoch 
+  window_size <- 10000
+  window_max <- ceiling(max_kb/window_size) 
+  
+  
+  ###inversion 
+  column_names_inv <- c()
+  row_names_inv <- c()
+  ld_matrix_inv <- matrix( 0, nrow = window_max, ncol = window_max)
+  
+  
+  window_x <- 1
+  window_y <- 1
+  for (window_x in 1:window_max) {
+    for (window_y in window_x:window_max) {
+      col_x_start <- (window_x - 1) * window_size + 1 
+      col_x_end <- (window_x) * window_size
+      
+      col_y_start <- (window_y - 1) * window_size + 1
+      col_y_end <- (window_y) * window_size
+      
+      #print(which(SNP_positions >= col_x_start & SNP_positions < col_x_end))
+      #print(which(SNP_positions >= col_y_start & SNP_positions < col_y_end))
+      x_indeces <- which(SNP_positions_inv >= col_x_start & SNP_positions_inv < col_x_end)
+      y_indeces <- which(SNP_positions_inv >= col_y_start & SNP_positions_inv < col_y_end)
+      ld_inv_cut <- ld_inv[y_indeces , x_indeces]
+      
+      if (length(x_indeces) == 0 | length(y_indeces) == 0) {
+        R2_2nd <- 0
+      } else {
+        R2_2nd <- sapply(ld_inv_cut, max)
+        R2_2nd <- sort(as.vector(R2_2nd), decreasing = T)
+        # print(c(window_x, window_y, R2_2nd))
+        if (length(R2_2nd) == 1) {
+          R2_2nd <- R2_2nd[1]
+        } else {
+          R2_2nd <- R2_2nd[2]
+        }
+      }
+      row_names_inv <- append(row_names_inv, paste(col_y_end))
+      row_names_inv <- unique(row_names_inv)
+      ld_matrix_inv[window_y, window_x] <- R2_2nd
+      ld_matrix_inv[window_x, window_y] <- R2_2nd
+      
+    }
+    column_names_inv <- append(column_names_inv, paste(col_x_end))
+  }
+  
+  colnames(ld_matrix_inv) <- c(column_names_inv)
+  rownames(ld_matrix_inv) <-c(row_names_inv)
+      
+      
+  ###whole contig
+  column_names_contig <- c()
+  row_names_contig <- c()
+  ld_matrix_contig <- matrix( 0, nrow = window_max, ncol = window_max)
+  
+  
+  window_x <- 1
+  window_y <- 1
+  for (window_x in 1:window_max) {
+    for (window_y in window_x:window_max) {
+      col_x_start <- (window_x - 1) * window_size + 1 
+      col_x_end <- (window_x) * window_size
+      
+      col_y_start <- (window_y - 1) * window_size + 1
+      col_y_end <- (window_y) * window_size
+      
+      #print(which(SNP_positions >= col_x_start & SNP_positions < col_x_end))
+      #print(which(SNP_positions >= col_y_start & SNP_positions < col_y_end))
+      x_indeces <- which(SNP_positions_contig >= col_x_start & SNP_positions_contig < col_x_end)
+      y_indeces <- which(SNP_positions_contig >= col_y_start & SNP_positions_contig < col_y_end)
+      ld_contig_cut <- ld_contig[y_indeces , x_indeces]
+      
+      if (length(x_indeces) == 0 | length(y_indeces) == 0) {
+        R2_2nd <- 0
+      } else {
+        R2_2nd <- sapply(ld_contig_cut, max)
+        R2_2nd <- sort(as.vector(R2_2nd), decreasing = T)
+        # print(c(window_x, window_y, R2_2nd))
+        if (length(R2_2nd) == 1) {
+          R2_2nd <- R2_2nd[1]
+        } else {
+          R2_2nd <- R2_2nd[2]
+        }
+      }
+      row_names_contig <- append(row_names_contig, paste(col_y_end))
+      row_names_contig <- unique(row_names_contig)
+      ld_matrix_contig[window_y, window_x] <- R2_2nd
+      ld_matrix_contig[window_x, window_y] <- R2_2nd
+      
+    }
+    column_names_contig <- append(column_names_contig, paste(col_x_end))
+  }
+  
+  colnames(ld_matrix_contig) <- c(column_names_contig)
+  rownames(ld_matrix_contig) <-c(row_names_contig)
+  
+  
+
+  
+  ld_matrix_final <- matrix( 0, nrow = window_max, ncol = window_max)
+  ld_matrix_final[upper.tri(ld_matrix_final)] <- ld_matrix_inv[upper.tri(ld_matrix_inv)]
+  ld_matrix_final[lower.tri(ld_matrix_final)] <-  ld_matrix_contig[lower.tri(ld_matrix_contig)]
+  
+  colnames(ld_matrix_final) <- c(column_names_inv)
+  rownames(ld_matrix_final) <-c(row_names_contig)
+  
+  write.table(ld_matrix_final, paste(path_to_output,'/',file_name_no_ext,'.txt', sep = ''), row.names = T, col.names = T)
+  
+}
